348dbe2c1adcfd038a108fa0702bf58749980248
nramiscal <nramiscal@gmail.com>
2017-11-27 12:38:28 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var app = express();
+var port = 8000;
+
+// this is the line that tells our server to use the "/static" folder for static content
+app.use(express.static(__dirname + "/static"));
+
+// This sets the location where express will look for the ejs views
+app.set('views', __dirname + '/views');
+// Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
+app.set('view engine', 'ejs');
+
+
+app.get('/', function(request, response){
+	response.send("<h1>Hello Express</h1>");
+})
+
+app.get("/users", function (request, response){
+    // hard-coded user data
+    var users_array = [
+        {name: "Michael", email: "michael@codingdojo.com"},
+        {name: "Jay", email: "jay@codingdojo.com"},
+        {name: "Brendan", email: "brendan@codingdojo.com"},
+        {name: "Andrew", email: "andrew@codingdojo.com"}
+    ];
+    response.render('users', {users: users_array});
+})
+
+
+app.listen(port, function(){
+	console.log(`listening on port ${port}`);
+})
