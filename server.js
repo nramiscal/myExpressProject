b695f77a277fed102dac1080c327629ecba62cc5
nramiscal <nramiscal@gmail.com>
2017-11-27 15:22:49 -0500

    first commit

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require("express");
 var app = express();
 var port = 8000;
+var users_array = [];
+
+var session = require('express-session');
+app.use(session({secret: 'thisissecret:P'}));  // string for encryption
+
+// require body-parser for using forms
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 // this is the line that tells our server to use the "/static" folder for static content
 app.use(express.static(__dirname + "/static"));
@@ -11,20 +20,29 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 
-app.get('/', function(request, response){
-	response.send("<h1>Hello Express</h1>");
-})
+app.get('/', function (req, res){
+  res.render('index', {title: "my Express project"});
+});
 
 app.get("/users", function (request, response){
-    // hard-coded user data
-    var users_array = [
-        {name: "Michael", email: "michael@codingdojo.com"},
-        {name: "Jay", email: "jay@codingdojo.com"},
-        {name: "Brendan", email: "brendan@codingdojo.com"},
-        {name: "Andrew", email: "andrew@codingdojo.com"}
-    ];
     response.render('users', {users: users_array});
 })
+
+// route to process new user form data:
+app.post('/users', function (req, res){
+  // console.log("POST DATA \n\n", req.body);
+  req.session.name = req.body.name;
+  // console.log(req.session.name);
+  users_array.push(req.body);
+  res.redirect('/users');
+})
+
+app.get("/users/:id", function (req, res){
+    console.log("The user id requested is:", req.params.id);
+    // just to illustrate that req.params is usable here:
+    res.send("You requested the user with id: " + req.params.id);
+    // code to get user from db goes here, etc...
+});
 
 
 app.listen(port, function(){
